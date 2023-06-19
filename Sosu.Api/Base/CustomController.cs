e802bc13913103cb827d3238af5c387de136e424
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-19 11:35:34 +0200

    Added a custom controller

---
+++ Sosu.Api/Base/CustomController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sosu.Api.Base;
+
+public abstract class CustomController<TService>
+    : ControllerBase
+    where TService : IService
+{
+    protected IService _service;
+
+    public CustomController(IService service)
+        => _service = service;
+}
