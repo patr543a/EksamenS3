22558da88012f2615a24966df8ce4a291d3c072f
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-19 13:56:03 +0200

    Implemented GetAllTasksFromResident and MarkTaskAsComplete

--- Sosu.Api/Services/TaskService.cs
+++ Sosu.Api/Services/TaskService.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Repository.Sosu;
 using Sosu.Api.Base;
 using Sosu.Api.Interfaces;
+using Task = Entities.Sosu.Task;
 
 namespace Sosu.Api.Services;
 
@@ -11,12 +12,19 @@ public class TaskService
     : ServiceBase<SosuUnitOfWork, SosuContext>, ITaskService
 {
     public IEnumerable<TaskDto> GetAllTasksFromResident(int residentId)
-    {
-        throw new NotImplementedException();
-    }
+        => _repositories
+            .TaskRepository
+            .Get(t => t.ResidentId == residentId, null, "CompletedByNavigation,Notes,Resident")
+            .Select(t =>
+            {
+                if (t.IsComplete)
+                    t.CompletedBy = null;
+
+                return t.ToDto();
+            });
 
     public ActionResult MarkTaskAsComplete(int employeeId, int taskId)
-    {
-        throw new NotImplementedException();
-    }
+        => _repositories
+            .TaskRepository
+            .MarkTaskAsComplete(employeeId, taskId);
 }
