c002763f57c6237c91a850a5c95fc198bc9447f1
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-19 13:54:48 +0200

    Added GetAllTasksFromResident and MarkTaskAsComplete methods

--- Sosu.Api/Controllers/TaskController.cs
+++ Sosu.Api/Controllers/TaskController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.Dto.Sosu;
+using Entities.Sosu;
+using Microsoft.AspNetCore.Mvc;
 using Sosu.Api.Base;
 using Sosu.Api.Interfaces;
+using Task = System.Threading.Tasks.Task;
 
 namespace Sosu.Api.Controllers;
 
@@ -12,5 +15,25 @@ public class TaskController
     public TaskController(ITaskService service)
         : base(service)
     {        
+    }
+
+    [HttpGet]
+    [Route("getFromResident")]
+    public async Task<ActionResult<IEnumerable<TaskDto>>> GetAllTasksFromResident(int? residentId)
+    {
+        if (residentId is null)
+            return BadRequest("Missing parameters");
+
+        return Ok(await Task.FromResult(_service.GetAllTasksFromResident((int)residentId)));
+    }
+
+    [HttpPut]
+    [Route("markAsComplete")]
+    public async Task<ActionResult> MarkTaskAsComplete(int? employeeId, int? taskId)
+    {
+        if (employeeId is null || taskId is null)
+            return BadRequest("Missing parameters");
+
+        return await Task.FromResult(_service.MarkTaskAsComplete((int)employeeId, (int)taskId));
     }
 }
