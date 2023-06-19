3072e3b2fdfcecef3d131b4f3b56b3896148d34b
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-19 13:56:16 +0200

    Added services to the API

--- Sosu.Api/Program.cs
+++ Sosu.Api/Program.cs
@@ -1,3 +1,6 @@
+using Sosu.Api.Interfaces;
+using Sosu.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<IResidentService, ResidentService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
