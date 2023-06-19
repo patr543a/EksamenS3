d85894728e24e5efd3aec7a46f2419bc1c35f292
patr543a <113888937+patr543a@users.noreply.github.com>
2023-06-19 13:53:06 +0200

    Added MarkTaskAsComplete method

--- Repository/Sosu/TaskRepository.cs
+++ Repository/Sosu/TaskRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Entities.Contexts;
 using Entities.Sosu;
 using Repository.Classes;
+using Microsoft.AspNetCore.Mvc;
 using TaskSosu = Entities.Sosu.Task;
+using Microsoft.AspNetCore.Http;
 
 namespace Repository.Sosu;
 
@@ -11,5 +13,20 @@ public class TaskRepository
     public TaskRepository(SosuContext context)
         : base(context)
     {
+    }
+
+    public ActionResult MarkTaskAsComplete(int employeeId, int taskId)
+    {
+        var task = GetByID(taskId);
+
+        if (task is null)
+            return new StatusCodeResult(StatusCodes.Status404NotFound);
+
+        task.IsComplete = true;
+        task.CompletedBy = employeeId;
+
+        Save();
+
+        return new StatusCodeResult(StatusCodes.Status200OK);
     }
 }
